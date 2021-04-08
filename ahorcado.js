cd7246d2ab64ad97be141893fcb3cd441b660cba
Octavio Caba <cabaoctavio@hotmail.com>
2021-04-08 14:31:06 -0300

    primera versión finalizada

--- ahorcado.js
+++ ahorcado.js
@@ -1,20 +1,23 @@
 const alphabet = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-const words = ['pilas', 'squall', 'final', 'minena'];
+const words = ['pilas', 'nubes', 'final', 'minena', 'tormenta', 'parapsicologia', 'pokemon', 'panfleto', 'mueca', 'pinguino', 'caracol', 'garfio', 'clarinete'];
 let mistakes = -1;
-const correctWords = [];
+const bodyParts = ['#torso', '#armL', '#armR', '#legL', '#legR'];
 
 const pickRandomWord = () => words[Math.floor(Math.random() * words.length)];
 
 $.when($.ready).then(() => {
+  startGame();
+});
+
+const startGame = () => {
+  $('.game-result').removeClass('winner'||'looser').text('');
+  $('.btn').removeClass('btn-inactive').attr('disabled', false);
+
   gsap.from('#head', {
     duration: 1,
     scale: 0.1,
     x: 15
   });
-
-  for (let i = 0; i < alphabet.length; i++) {
-    $('#keyboard').append(`<input type="button" class="btn" value="${alphabet[i]}">`); 
-  }
 
   let word = pickRandomWord().toUpperCase();
   for (let i = 0; i < word.length; i++) {
@@ -25,30 +28,41 @@ $.when($.ready).then(() => {
     );
   }
 
+  for (let i = 0; i < alphabet.length; i++) {
+    $('#keyboard').append(`<input type="button" class="btn" value="${alphabet[i]}">`); 
+  }
+
   $('.btn').on('click', e => {
+    $(e.target).addClass('btn-inactive').attr('disabled', true);
+
     if (word.includes(e.target.defaultValue)) {
-      $('.'+ e.target.defaultValue).css('visibility', 'initial');
-      correctWords.push(e.target.defaultValue);
+      $('.'+ e.target.defaultValue).addClass('letter-guessed');
       hasWon(word);
     } else {
       mistakeMade();
     }
-
-    $(e.target).addClass('btn-inactive').attr('disabled', true);
   });
+}
+
+$('.restart-btn').on('click', () => {
+  $('.letter-field').remove();
+  $('.btn').remove();
+  $('.hidden-bodypart').css('display', 'none');
+  mistakes = -1;
+  startGame();
 });
 
-const hasWon = (word) => {
-  if (word.includes(correctWords)) {
-    console.log('Palabra completa');
-  } else {
-    console.log('Palabra incompleta');
+const hasWon = word => {
+  if ($('.letter-guessed').length === word.length) {
+    $('.btn').addClass('btn-inactive').attr('disabled', true);
+    $('.game-result').addClass('winner').text('Ganaste');
+    $('.letter').css( 'color', 'forestgreen');
   }
 }
 
-const bodyParts = ['#torso', '#armL', '#armR', '#legL', '#legR'];
 const mistakeMade = () => {
   mistakes++;
+
   if (mistakes < 5) {
     $(bodyParts[mistakes]).show();
     gsap.from(bodyParts[mistakes], {
@@ -57,10 +71,8 @@ const mistakeMade = () => {
       y: 20
     }); 
   } else {
-    alert('game-over');
+    $('.letter').css({ 'visibility': 'initial', 'color': 'red' });
+    $('.btn').addClass('btn-inactive').attr('disabled', true);
+    $('.game-result').addClass('looser').text('Perdiste');
   }
 }
-
-$('#botoncito').on('click', () => {
-  console.log('funciona');
-});
